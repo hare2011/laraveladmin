8ee92882c971791777e36189121a200415ffb10c
hare <hare@heavengifts.com>
2017-11-03 10:52:30 +0800

    add Signed-off-by:hare <hare@heavengifts.com>

---
+++ views/form/baiduwebuploader.blade.php
@@ -0,0 +1,34 @@
+<div class="form-group {!! !$errors->has($errorKey) ?: 'has-error' !!}">
+
+    <label for="{{$id}}" class="col-sm-{{$width['label']}} control-label">{{$label}}</label>
+
+    <div class="col-sm-{{$width['field']}}">
+
+        @include('admin::form.error')
+
+
+        <!--dom结构部分-->
+        <div id="uploader-hare">
+            <!--用来存放item-->
+            <div id="fileList" class="uploader-list" style="height: 260px;border:1px solid #eee; clear: both;">
+                @if($value)
+                @foreach($value as $key=>$v)
+                <div id="OLDWU_FILE_0{{$loop->index}}" class="file-item thumbnail">
+                    <img src="{{config('admin.upload.host')}}{{$v}}">
+                    <div class="file-panel" storageid="OLDWU_FILE_{{$loop->index}}" style="display: none;"><span class="cancel">删除</span></div>
+                    <input id="IOLDWU_FILE_{{$loop->index}}" type="hidden"  name="imageid[]" value="{{$key}}"/>
+                    
+                </div>
+                @endforeach
+                @endif
+            </div>
+            <div id="filePicker" style="margin-top:10px;">选择图片</div>
+        </div>
+
+
+        <!--input type="file" class="{{$class}}" name="{{$name}}" {!! $attributes !!} /-->
+
+        @include('admin::form.help-block')
+
+    </div>
+</div>
