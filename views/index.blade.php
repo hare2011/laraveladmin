7d2ec12ec79ff736ea633927fb80898828e93c53
hare <hare@heavengifts.com>
2017-11-07 09:34:56 +0800

    add nprogress

--- views/index.blade.php
+++ views/index.blade.php
@@ -16,6 +16,7 @@
 
     {!! Admin::css() !!}
     <link rel="stylesheet" href="{{ asset("/packages/admin/nestable/nestable.css") }}">
+    <link rel="stylesheet" href="{{ asset("/packages/admin/nprogress/nprogress.css") }}">
     <link rel="stylesheet" href="{{ asset("/packages/admin/toastr/build/toastr.min.css") }}">
     <link rel="stylesheet" href="{{ asset("/packages/admin/bootstrap3-editable/css/bootstrap-editable.css") }}">
     <link rel="stylesheet" href="{{ asset("/packages/admin/google-fonts/fonts.css") }}">
@@ -53,79 +54,20 @@
 
 <!-- ./wrapper -->
 
+<script>
+    function LA() {}
+    LA.token = "{{ csrf_token() }}";
+</script>
+
+
 <!-- REQUIRED JS SCRIPTS -->
 <script src="{{ asset ("/packages/admin/AdminLTE/plugins/chartjs/Chart.min.js") }}"></script>
 <script src="{{ asset ("/packages/admin/nestable/jquery.nestable.js") }}"></script>
 <script src="{{ asset ("/packages/admin/toastr/build/toastr.min.js") }}"></script>
 <script src="{{ asset ("/packages/admin/bootstrap3-editable/js/bootstrap-editable.min.js") }}"></script>
-
+<script src="{{ asset ("/packages/admin/nprogress/nprogress.js") }}"></script>
 {!! Admin::js() !!}
-
-<script>
-
-    function LA() {}
-    LA.token = "{{ csrf_token() }}";
-
-    $.fn.editable.defaults.params = function (params) {
-        params._token = '{{ csrf_token() }}';
-        params._editable = 1;
-        params._method = 'PUT';
-        return params;
-    };
-
-    toastr.options = {
-        closeButton: true,
-        progressBar: true,
-        showMethod: 'slideDown',
-        timeOut: 4000
-    };
-
-    $.pjax.defaults.timeout = 5000;
-    $.pjax.defaults.maxCacheLength = 0;
-    $(document).pjax('a:not(a[target="_blank"])', {
-        container: '#pjax-container'
-    });
-
-    $(document).on('submit', 'form[pjax-container]', function(event) {
-        $.pjax.submit(event, '#pjax-container')
-    });
-
-    $(document).on("pjax:popstate", function() {
-
-        $(document).one("pjax:end", function(event) {
-            $(event.target).find("script[data-exec-on-popstate]").each(function() {
-                $.globalEval(this.text || this.textContent || this.innerHTML || '');
-            });
-        });
-    });
-    
-    $(document).on('pjax:send', function(xhr) {
-        if(xhr.relatedTarget && xhr.relatedTarget.tagName && xhr.relatedTarget.tagName.toLowerCase() === 'form') {
-            $submit_btn = $('form[pjax-container] :submit');
-            if($submit_btn) {
-                $submit_btn.button('loading')
-            }
-        }
-    })
-    
-    $(document).on('pjax:complete', function(xhr) {
-        if(xhr.relatedTarget && xhr.relatedTarget.tagName && xhr.relatedTarget.tagName.toLowerCase() === 'form') {
-            $submit_btn = $('form[pjax-container] :submit');
-            if($submit_btn) {
-                $submit_btn.button('reset')
-            }
-        }
-    })
-
-    $(function(){
-        $('.sidebar-menu li:not(.treeview) > a').on('click', function(){
-            var $parent = $(this).parent().addClass('active');
-            $parent.siblings('.treeview.active').find('> a').trigger('click');
-            $parent.siblings().removeClass('active').find('li').removeClass('active');
-        });
-    });
-
-</script>
+<script src="{{ asset ("/packages/admin/laravel-admin/laravel-admin.js") }}"></script>
 
 </body>
 </html>
