03b49f489838aba63ca8b89013a80681ae9cb07e
hare2011 <yetuwo@qq.com>
2018-07-09 11:43:19 +0800

    fix bug

--- views/form.blade.php
+++ views/form.blade.php
@@ -25,7 +25,7 @@
         <!-- /.box-body -->
         <div class="box-footer">
 
-            @if( ! $form->isMode(\Encore\Admin\Form\Builder::MODE_VIEW)  || ! $form->option('enableSubmit'))
+            @if( ! $form->isMode(\Runhare\Admin\Form\Builder::MODE_VIEW)  || ! $form->option('enableSubmit'))
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
             @endif
             <div class="col-sm-{{$width['label']}}">
